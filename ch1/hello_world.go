5a95bd5a21951492ee2773620ffe9c078464ee2d
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-04-27 07:42:33 +0530

    hello world maybe

---
+++ ch1/hello_world.go
@@ -0,0 +1,9 @@
+package main //package main is special as it defines a standalone program, not a library
+
+import (
+	"fmt" //the fmt package contains functions for printing formatted output and scanning input
+)
+
+func main() { //this is where the execution of the program begins
+			fmt.Println("Hello, world?")
+}
