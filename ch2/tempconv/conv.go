0f5c329495fcc8a3b43dbd03aa8e6843b83965b1
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-01 16:00:53 +0530

    ex 2.1, tempconv package

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -2,4 +2,12 @@ package tempconv
 
 func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
+func CtoK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
 func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+func KtoC(k Kelvin) Celsius { return Celsius(k - 273.5) }
+
+func KtoF(k Kelvin) Fahrenheit { return CtoF(KtoC(k)) }
+
+func FtoK(f Fahrenheit) Kelvin { return CtoK(FtoC(f)) }
--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -6,6 +6,7 @@ import (
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
@@ -13,5 +14,6 @@ const (
 	BoilingC      Celsius = 100
 )
 
+func (k Kelvin) String() string     { return fmt.Sprintf("%g K", k) }
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
