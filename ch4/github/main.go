a267baca0394dde591a937e987434ee05537a50c
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-08 09:10:17 +0530

    ex4.10 issues age

--- ch4/github/main.go
+++ ch4/github/main.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 	"gopl/ch4/github/github"
 	"os"
+	"time"
 )
+
+type TimeIssue struct {
+	Time  string
+	Items []github.Issue
+}
+
+func printIssues(issue TimeIssue) {
+	fmt.Printf("\n%s:\n", issue.Time)
+	for _, item := range issue.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n",
+			item.Number, item.User.Login, item.Title)
+	}
+
+}
 
 func main() {
 	result, err := github.SearchIssues(os.Args[1:])
@@ -12,9 +27,22 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	var monthOld, lessThanYearOld, old TimeIssue
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+		if time.Now().Year()-item.CreatedAt.Year() > 1 {
+			old.Items = append(old.Items, *item)
+		} else if time.Now().Month() == item.CreatedAt.Month() {
+			monthOld.Items = append(monthOld.Items, *item)
+		} else {
+			lessThanYearOld.Items = append(lessThanYearOld.Items, *item)
+		}
 	}
+	monthOld.Time = "Recent issues"
+	lessThanYearOld.Time = "Issues less than a year old"
+	old.Time = "Old issues"
+	printIssues(monthOld)
+	printIssues(lessThanYearOld)
+	printIssues(old)
+	fmt.Println()
 }
--- ch4/omdb/main.go
+++ ch4/omdb/main.go
@@ -14,41 +14,41 @@ const omdbURL = "http://www.omdbapi.com/?apikey=[your api key here]&t="
 
 func main() {
 	q := strings.Join(os.Args[1:], "+")
-	r, err:= http.Get(omdbURL+q)
-	if err!=nil{
+	r, err := http.Get(omdbURL + q)
+	if err != nil {
 		fmt.Printf("Error occured %v", err)
 	}
-	if r.StatusCode!=http.StatusOK{
+	if r.StatusCode != http.StatusOK {
 		r.Body.Close()
 		fmt.Println("Error", r.Status)
 	}
 	var result MovieSearchResult
 	if err := json.NewDecoder(r.Body).Decode(&result); err != nil {
 		r.Body.Close()
-    }
+	}
 	fmt.Print("Fetched\n")
 	r.Body.Close()
 	fmt.Println(result.Poster)
-	r, err= http.Get(result.Poster)
-	if err!=nil{
+	r, err = http.Get(result.Poster)
+	if err != nil {
 		fmt.Println("Error occurred while fetching the poster", err)
 		os.Exit(1)
 	}
 	fmt.Printf("Fetched the poster\n")
 	file, err := os.Create(fmt.Sprintf("tmp/%s.jpg", q))
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    _, err = io.Copy(file, r.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Success!")
+	_, err = io.Copy(file, r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Success!")
 	r.Body.Close()
 }
 
-type MovieSearchResult struct{
+type MovieSearchResult struct {
 	Poster string
 }
