1d674a903d3d99e6c03ff9e4e3c00dec9d2fbe4f
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-12 10:14:54 +0530

    ex6.4

--- ch6/intset/intset.go
+++ ch6/intset/intset.go
@@ -136,3 +136,16 @@ func (s IntSet) String() string {
 	buf.WriteByte('}')
 	return buf.String()
 }
+
+//Elems() method
+func (s *IntSet) Elems() []int {
+	elems := make([]int, 0)
+	for idx, word := range s.words {
+		for j := 0; j < 64; j++ {
+			if word&(1<<j) != 0 {
+				elems = append(elems, idx*64+j)
+			}
+		}
+	}
+	return elems
+}
