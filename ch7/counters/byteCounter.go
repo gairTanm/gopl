82908916c5de164901e7e862e8a21a8386537059
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-13 07:02:14 +0530

    ex7.2

--- ch7/counters/byteCounter.go
+++ ch7/counters/byteCounter.go
@@ -1,8 +1,19 @@
 package counters
 
-type ByteCounter int
+import "io"
+
+type ByteCounter struct {
+	w       io.Writer
+	written int64
+}
 
 func (c *ByteCounter) Write(p []byte) (int, error) {
-	*c += ByteCounter(len(p))
-	return len(p), nil
+	n, err := c.w.Write(p)
+	c.written += int64(n)
+	return n, err
+}
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	c := &ByteCounter{w, 0}
+	return c, &c.written
 }
