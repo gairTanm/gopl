e6cb04f67aab1973135f7d797335c7b39962dfc5
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-17 18:43:03 +0530

    ex7.6

--- ch7/tempconv/conv.go
+++ ch7/tempconv/conv.go
@@ -18,7 +18,8 @@ const (
 func (k Kelvin) String() string     { return fmt.Sprintf("%g K", k) }
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
-func CtoF(c Celsius) Fahrenheit     { return Fahrenheit(c*9/5 + 32) }
+
+func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 func CtoK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
 
@@ -43,6 +44,9 @@ func (f *celsiusFlag) Set(s string) error {
 	case "F", "°F":
 		f.Celsius = FtoC(Fahrenheit(value))
 		return nil
+	case "K":
+		f.Celsius = KtoC(Kelvin(value))
+		return nil
 	}
 	return fmt.Errorf("invalid temperature %q", s)
 }
@@ -52,5 +56,3 @@ func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
 	flag.CommandLine.Var(&f, name, usage)
 	return &f.Celsius
 }
-
-var temp = CelsiusFlag("t", 24.0, "temperature")
